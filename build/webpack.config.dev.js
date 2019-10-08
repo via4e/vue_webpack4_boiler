1618e68197bfea9ecd7debd685365600457ee787
via4e <version-1@yandex.ru>
2019-10-08 23:38:44 +0300

    init commit

---
+++ build/webpack.config.dev.js
@@ -0,0 +1,56 @@
+'use strict'
+const webpack = require('webpack')
+const { VueLoaderPlugin } = require('vue-loader')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const path = require('path')
+
+module.exports = {
+  mode: 'development',
+  devServer: {
+    hot: true,
+    watchOptions: {
+      poll: true
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|vue)$/,
+        use: 'eslint-loader',
+        enforce: 'pre'
+      },          {
+        test: /\.vue$/,
+        use: 'vue-loader'
+      },
+      {
+        test: /\.js$/,
+        use: 'babel-loader'
+      },       
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+            'vue-style-loader',
+            'css-loader',
+            'sass-loader'
+        ]
+      }      
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'index.html',
+      inject: true
+    })
+  ]
+}
---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="full-width center-content">
+    <h1 class="test">
+      App
+    </h1>
+  </div>
+</template>
